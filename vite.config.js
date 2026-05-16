e05f9ee137366313f1566f35a99042f7ae597fb2
Azel <your_email@example.com>
2026-05-16 18:38:37 +0800

    +

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/sci-hub/', // Replace with your actual repo name
+  base: '/', // Must be '/' for Vercel
 })
