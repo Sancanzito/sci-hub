c37506b90c3554e2bd3a683080149227ae27d74a
Azel <your_email@example.com>
2026-05-16 17:44:57 +0800

    repo

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/sci-hub/', // Replace with your actual repo name
 })
